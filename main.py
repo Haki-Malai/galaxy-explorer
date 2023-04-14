59021baa72951e3d7ad6a43a8ffee4013e2f3c6d
Haki-Malai <p17mala@ionio.gr>
2023-04-14 16:08:34 +0300

    🔨 refactor(main.py): refactor main.py to use click for command line interface ✨ feat(main.py): add verbose flag to search command in main.py

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
-#!/usr/bin/env python3
-
-import requests
-import sys
+import click
+from starwars_api import StarWarsAPI
 
 
-class StarWarsAPI:
-    BASE_URL = "https://www.swapi.tech/api/"
-
-    def generate_character_data(self, name):
-        """Generates data for a character in the Star Wars API by name"""
-        url = f"{self.BASE_URL}people/?name={name}"
-        response = requests.get(url)
-
-        if response.status_code != 200:
-            print("Error: Could not reach API.")
-            return
-
-        json_data = response.json()
-        results = json_data["result"]
-        if not results:
-            print("The force is not strong within you.")
-            return
-
-        for character in results:
-            properties = character["properties"]
-            yield (
-                f"Name: {properties['name']}\n"
-                f"Height: {properties['height']}\n"
-                f"Mass: {properties['mass']}\n"
-                f"Birth year: {properties['birth_year']}"
-            )
-
-    def print_character_data(self, name):
-        """Prints data for a character in the Star Wars API by name"""
-        for data in self.generate_character_data(name):
-            print('\n' + data)
+@click.group()
+def main():
+    """A command line interface for the Star Wars API."""
+    pass
 
 
-if __name__ == "__main__":
+@main.command()
+@click.argument('name')
+@click.option('-v', '--verbose', is_flag=True, help='Raise errors/warnings.')
+def search(name, verbose):
+    """Search for a character in the Star Wars API.
+    """
     api = StarWarsAPI()
-    if len(sys.argv) == 3 and sys.argv[1] == "search":
-        name = sys.argv[2]
-        api.print_character_data(name)
-    else:
-        print("Usage: ./main.py search [character name]")
+    api.print_character_data(name, verbose)
+
+
+if __name__ == '__main__':
+    main()
